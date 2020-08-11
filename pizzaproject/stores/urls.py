724b09be74eff9241d2d0a05be4cf9d7ec256ec5
programwithus <learntocodewithus@gmail.com>
2020-08-11 14:38:48 -0400

    final version

--- pizzaproject/stores/urls.py
+++ pizzaproject/stores/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PizzeriaListAPIView.as_view(), name='pizzeria_list'),
+    path('listings/', views.PizzeriaListAPIView.as_view(), name='pizzeria_list'),
     path('<int:id>/', views.PizzeriaRetrieveAPIView.as_view(),
          name='pizzeria_detail'),
     path('create/', views.PizzeriaCreateAPIView.as_view(), name='pizzeria_create'),
