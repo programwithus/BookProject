d1f8af636a72d07a5b4f858db64dc03870d16e6b
programwithus <learntocodewithus@gmail.com>
2020-08-11 14:15:43 -0400

    final version

--- pizzaproject/pizzaproject/urls.py
+++ pizzaproject/pizzaproject/urls.py
@@ -23,7 +23,7 @@ from stores.views import UserCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stores.urls')),
+    path('api/', include('stores.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('register/', UserCreateView.as_view(), name='create_user'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pizzaproject/stores/serializers.py
+++ pizzaproject/stores/serializers.py
@@ -10,7 +10,6 @@ UserModel = get_user_model()
 
 class PizzeriaListSerializer(serializers.ModelSerializer):
     absolute_url = serializers.SerializerMethodField()
-    image_url = serializers.SerializerMethodField('get_image_url')
 
     class Meta:
         model = Pizzeria
@@ -27,27 +26,18 @@ class PizzeriaListSerializer(serializers.ModelSerializer):
     def get_absolute_url(self, obj):
         return reverse('pizzeria_detail', args=(obj.pk,))
 
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(obj.logo_image.url)
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
-    image_url = serializers.SerializerMethodField('get_image_url')
 
     class Meta:
-        fields = ['id','image', 'image_title', 'uploded_at', 'image_url']
+        fields = ['id','image', 'image_title', 'uploded_at']
         model = Image
-
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(obj.image.url)
 
 
 class PizzeriaDetailSerializer(serializers.ModelSerializer):
     update = serializers.SerializerMethodField()
     delete = serializers.SerializerMethodField()
     pizzeria_images = ImageSerializer(many=True,required=False)
-    image_url = serializers.SerializerMethodField('get_image_url')
 
     class Meta:
         model = Pizzeria
@@ -67,7 +57,6 @@ class PizzeriaDetailSerializer(serializers.ModelSerializer):
             'update',
             'delete',
             'pizzeria_images',
-            'image_url',
         ]
 
     def get_update(self, obj):
@@ -76,9 +65,6 @@ class PizzeriaDetailSerializer(serializers.ModelSerializer):
     def get_delete(self, obj):
         return reverse('pizzeria_delete', args=(obj.pk,))
 
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(obj.logo_image.url)
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
